b9a53f838e60c201c9be18abd87563006e0d12e6
PERLA TAPIA UREÑA <Alis@PERLAs-MacBook-Pro.local>
2016-12-07 17:59:29 -0600

    version1.4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,53 +22,18 @@ $("#addTrainBtn").on("click", function() {
   // Grabs user input
   var trnName = $("#trainNameInput").val().trim();
   var trnDest = $("#destination-input").val().trim();
-  var trnFirst = moment($("#firstTrtainTime").val().trim(), "HH:mm");
+  var trnFirst = moment($("#firstTrtainTime").val().trim(), "HH:mm").format("X");
   var trnFreq = $("#frequency-input").val().trim();
 
-  
-
-  // Creates local "temporary" object for holding train data
-  var newTrain = {
-    name: trnName,
-    dest: trnDest,
-    first: trnFirst,
-    freq: trnFreq,
-    // tMinutesTillTrain:tMinutesTillTrain,
-    // nextArrival:nextArrival,
-    // dateAdded:firebase.database.ServerValue.TIMESTAMP,
-
-  };
-
-  // Uploads train data to the database
-  database.ref().push(newTrain);
-
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.dest);
-  console.log(newTrain.first);
-  console.log(newTrain.freq);
-  // console.log(newTrain.tMinutesTillTrain);
-  // console.log(newTrain.nextArrival);
-  // console.log(newTrain.dateAdded);
-
-
-
-  // Alert
-  alert("Train successfully added");
-
-  // Clears all of the text-boxes
-  $("#trainNameInput").val("");
-  $("#destination-input").val("");
-  $("#firstTrtainTime").val("");
-  $("#frequency-input").val("");
-// First Time (pushed back 1 year to make sure it comes before current time)
-      var firstTimeConverted = moment(trnFirst, "HH:mm").subtract(1, "years"); // find out more about .substract(1 ...)
+  // First Time (pushed back 1 year to make sure it comes before current time)
+      var firstTimeConverted = moment.unix(trnFirst).subtract(1, "years"); // find out more about .substract(1 ...)
       console.log(firstTimeConverted);
       // Current Time
       var currentTime = moment();
       console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
       // Difference between the times
-      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      var diffTime = moment().diff(firstTimeConverted, "minutes");
+    
       console.log("DIFFERENCE IN TIME: " + diffTime);
 
       // Time apart (remainder)
@@ -84,9 +49,42 @@ $("#addTrainBtn").on("click", function() {
       console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
       // Arrival time
-  	  nextArrival = moment(nextTrain).format("HH:mm a");
+      nextArrival = moment(nextTrain).format("HH:mm a");
 
-  // Prevents moving to new page
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    name: trnName,
+    dest: trnDest,
+    first: trnFirst,
+    freq: trnFreq,
+    tMinutesTillTrain: tMinutesTillTrain,
+    nextArrival: nextArrival,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP,
+
+  };
+
+  // Uploads train data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.dest);
+  console.log(newTrain.first);
+  console.log(newTrain.freq);
+  console.log(newTrain.tMinutesTillTrain);
+  console.log(newTrain.nextArrival);
+  console.log(newTrain.dateAdded);
+
+  // Alert
+  alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  $("#trainNameInput").val("");
+  $("#destination-input").val("");
+  $("#firstTrtainTime").val("");
+  $("#frequency-input").val("");
+
+ // Prevents moving to new page
   return false;    
 
 });
@@ -102,8 +100,8 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   var trnDest = childSnapshot.val().dest;
   var trnFirst = childSnapshot.val().first;
   var trnFreq = childSnapshot.val().freq;
-  // var tMinutesTillTrain = childSnapshot.val().tMinutesTillTrain;
-  // var nextArrival = childSnapshot.val().nextArrival;
+  var tMinutesTillTrain = childSnapshot.val().tMinutesTillTrain;
+  var nextArrival = childSnapshot.val().nextArrival;
 
   
 
@@ -119,9 +117,9 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   // Add each train's data into the table
   $("#train-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDest + "</td><td>" +
-  trnFreq + "</td>      </tr>");
+  trnFreq + "</td>   <td>" + nextArrival + "</td><td>" + tMinutesTillTrain + "</td>    </tr>");
 
-  //<td>" + nextArrival + "</td><td>" + tMinutesTillTrain + "</td> 
+  
 
 });
 
